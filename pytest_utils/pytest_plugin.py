d80faf8e60ef3c0d94d0bca9f7b22956717320a6
Nicholas DeMarinis <ndemarinis@wpi.edu>
2023-04-09 08:52:50 -0400

    Updated to include test with errors and include last line of error for feedback.

--- pytest_utils/pytest_plugin.py
+++ pytest_utils/pytest_plugin.py
@@ -12,24 +12,35 @@ def pytest_terminal_summary(terminalreporter, exitstatus):
     json_results = {'tests': []}
 
     all_tests = []
+    #import pdb; pdb.set_trace()
+    def _add(all_tests, new_tests, is_error):
+        for t in new_tests:
+            t.is_error = is_error
+
+        all_tests.extend(new_tests)
+
     if ('passed' in terminalreporter.stats):
-        all_tests = all_tests + terminalreporter.stats['passed']
+        _add(all_tests, terminalreporter.stats['passed'], is_error=False)
     if ('failed' in terminalreporter.stats):
-        all_tests = all_tests + terminalreporter.stats['failed']
+        _add(all_tests, terminalreporter.stats['failed'], is_error=True)
+    if ('error' in terminalreporter.stats):
+        _add(all_tests, terminalreporter.stats['error'], is_error=True)
 
     for s in all_tests:
         output = ''
         score = s.max_score
         if (s.outcome == 'failed'):
             score = 0
-            output = str(s.longrepr.chain[0][0].reprentries[0])
+            output = s.longrepr.reprcrash.__str__() if s.is_error else "Detailed output is hidden."
+            #output = str(s.longrepr.chain[0][0].reprentries[0])
 
         json_results["tests"].append(
             {
                 'score': score,
                 'max_score': s.max_score,
                 'name': s.location[2],
-                'output': "Detailed output is hidden.", # hotfix: used to be output, wwhich leaked tests.
+                #'output': "Suppressed output.",
+                'output': output, #TODO handle this in autograde.py instead so we have more options?
                 'visibility': s.visibility
             }
         )
